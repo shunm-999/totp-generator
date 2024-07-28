b4e626c61fe0a776fa96a652caaa5e74b28e297e
shunm-999 <shunmiyazaki7812@gmail.com>
2024-07-28 18:04:38 +0900

    export設定を修正

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,6 @@ import {Hotp, HotpOption} from "./hotp";
 import {Totp, TotpOption} from "./totp";
 import {SecretGenerator} from "./lib/generate_secret";
 import {HashAlgorithm} from "./lib/hash_algorithm";
+import OtpAuthUri from "./otp_auth_uri";
 
-export {SecretGenerator, HashAlgorithm, Hotp, HotpOption, Totp, TotpOption};
+export {SecretGenerator, HashAlgorithm, Hotp, HotpOption, Totp, TotpOption, OtpAuthUri};
